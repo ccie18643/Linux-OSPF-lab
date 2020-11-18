8e676817bf7749101bf72b544807435348bc5244
1pig2far <1pig2far@gmail.com>
2020-11-18 18:23:34 -0500

    Added new files

---
+++ router.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+hostname=$(hostname)
+id=${hostname:1}
+
+if [ -z $(pidof watchfrr) ];
+then 
+    ip netns add router
+
+    ip link set dev enp6s0 netns router
+
+    ip netns exec router sysctl net.ipv4.conf.all.forwarding=1 > /dev/null
+    ip netns exec router sysctl net.ipv4.conf.default.forwarding=1 > /dev/null
+    ip netns exec router sysctl net.ipv6.conf.all.forwarding=1 > /dev/null
+    ip netns exec router sysctl net.ipv6.conf.default.forwarding=1 > /dev/null
+
+    ip netns exec router ip link set lo up
+    ip netns exec router ip link set enp6s0 up
+
+    for network in "${@}"
+    do
+        ip netns exec router ip link add link enp6s0 name vlan${network} type vlan id ${network}
+        ip netns exec router ip link set vlan${network} up
+        ip netns exec router ip address add 10.0.${network}.${id}/24 dev vlan${network}
+    done
+
+    ip netns exec router /usr/lib/frr/frrinit.sh start
+fi
+
+ip netns exec router bash
+
+
