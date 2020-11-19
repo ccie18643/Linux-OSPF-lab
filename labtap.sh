335e9ed7fe01e36f276faaaf8fe45a2c4860b4f3
1pig2far <1pig2far@gmail.com>
2020-11-19 18:48:39 -0500

    labtap interface fo wireshark

--- labtap.sh
+++ labtap.sh
@@ -9,3 +9,4 @@ ovs-vsctl add-port ovs-br0 labtap
 ovs-vsctl -- --id=@labtap get Port labtap \
           -- --id=@m create mirror name=0 select-all=true output-port=@labtap \
           -- set Bridge ovs-br0 mirrors=@m
+
